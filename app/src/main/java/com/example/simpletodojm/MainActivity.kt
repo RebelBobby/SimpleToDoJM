824463741a9979476bdf72467a27045fad137ec1
Toby Mc <joseph.matulewicz@gmail.com>
2021-12-16 17:12:29 -0800

    beginning of task item adapter video 2 12:00

--- app/src/main/java/com/example/simpletodojm/MainActivity.kt
+++ app/src/main/java/com/example/simpletodojm/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.simpletodojm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+    val listOfTasks = mutableListOf<String>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+ /*       findViewById<Button>(R.id.button).setOnClickListener{
+            Log.i("Joe", "User clicked a button")
+
+        }
+ */
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Boogie Board")
     }
 }
---
+++ app/src/main/java/com/example/simpletodojm/TaskItemAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.simpletodojm
+
+/*
+A bridge that tells recy
+ */
+class TaskItemAdapter {
+}
